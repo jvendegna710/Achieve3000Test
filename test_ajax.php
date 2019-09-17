8ef48fcc24eb27b1c70037ea7a4e6a1564f4007e
Jesse Vendegn <jvendegna710@gmail.com>
2019-09-17 17:51:16 -0400

    Update test_ajax.php

--- test_ajax.php
+++ test_ajax.php
@@ -1,13 +1,13 @@
 <?php
-if (!empty($_POST['color'])){ //this is for Quiz and Quiz Results
-    $color = $_POST['color'];     
-    $sql = "SELECT SUM(VOTES) AS VTOTAL  FROM VOTES WHERE COLOR = ".$_POST['color'];
-    $result = $db->ExecSQL($sql,$user);    
-    $returnArray = array("vtotal"=>$result[0]['VTOTAL']);
-    
+include_once("../database.php.inc");
+global $db;
+
+if (!empty($_GET['color'])){ 
+    $color = $_GET['color'];     
+    $sql = "SELECT SUM(VOTES) AS VTOTAL  FROM JUDGMENTS_TE_TEST3 WHERE COLOR ='{$color}'";
+    $result = $db->ExecSQL($sql);    
+    $vtotal= $result[0]['VTOTAL'];   
      
-}else $returnArray=array("vtotal"=>'FAILED AJAX CALL');
-// encoding array to json format
-echo json_encode($returnArray);
-/** */
+}else {$vtotal='ERROR: FAILED AJAX CALL';}
+echo $vtotal;
 ?>
